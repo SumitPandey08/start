d26702cb1199c3f8f64552a7f952cbe6c6b6cfee
SumitPandey08 <sp.sumitpandey06@gmail.com>
2024-07-31 13:11:38 +0530

    Update Commit

---
+++ script.js
@@ -0,0 +1,36 @@
+let slideIndex = 1;
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    const slides = document.getElementsByClassName("mySlides");
+    const dots = document.getElementsByClassName("dot");
+
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+
+    // Hide all slides
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    // Show the current slide
+    slides[slideIndex - 1].style.display = "block";
+
+    // Update active dot
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    dots[slideIndex - 1].className += " active";
+}
+
+// Call showSlides(1) to display the first slide
+showSlides(1);
